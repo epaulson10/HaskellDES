a4f530cbc22cec9d32e405929480caf62bd43afb
Erik Paulson <epaulson10@gmail.com>
2017-06-06 00:03:57 -0700

    Create an executable that encrypts/decrypts files
    This commit adds an executable that consumes the DES module to encrypt decrypt files.
    It processes the input file, adding padding to ensure that all blocks of the plaintext
    are 8 bytes in length. It uses the Crypto.Hash.SHA256 module to generate a key from a
    password by hashing the password and taking the first 64 bits to be a key. This likely is
    not secure, but it is easy to do for this project. Note that in DES, only 56 of those bits
    are considered to be the key, the rest are parity bits.

---
+++ main.hs
@@ -0,0 +1,90 @@
+import System.Environment
+import System.IO
+import System.Directory
+import System.Exit
+import DES
+import Crypto.Hash.SHA256 ( hash )
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString as B
+
+main :: IO ()
+main = do
+    args <- getArgs
+    (command,infile,outfile) <- parseArgs args
+    password <- getPassword
+    fileContents <- B.readFile infile
+    let key = BC.take 64 $ hash $ BC.pack password 
+        process = if command == "encrypt" then encryptByteString else decryptByteString
+        in  B.writeFile outfile $ process key fileContents
+    return ()
+
+encryptByteString :: B.ByteString -> B.ByteString -> B.ByteString
+encryptByteString key contents =  (B.concat .  map (`encrypt` key) . pad . chunk) contents
+decryptByteString :: B.ByteString -> B.ByteString -> B.ByteString
+decryptByteString key contents = (B.concat . unpad . map (`decrypt` key) . chunk) contents
+
+parseArgs :: [String] -> IO (String, String, String)
+parseArgs [cmd, infile, outfile] = do
+    inputExists <- doesFileExist infile
+    if (cmd /= "encrypt" && cmd /= "decrypt") || not inputExists then
+        usage >> exitFailure
+    else
+        return (cmd, infile, outfile)
+parseArgs _ = usage >> exitFailure
+
+    
+
+usage :: IO()
+usage = do
+    progName <- getProgName 
+    putStrLn $ progName ++ " [encrypt,decrypt] infile outfile"
+
+getPassword :: IO String
+getPassword = do
+    putStr "Enter the password: "
+    oldEchoVal <- hGetEcho stdin
+    hSetEcho stdin False
+    pass <- getLineSecret
+    putChar '\n'
+    putStr "Reenter the password: "
+    pass2 <- getLineSecret
+    putChar '\n'
+    hSetEcho stdin oldEchoVal
+    if pass /= pass2 then do
+        putStrLn "*Passwords didn't match*"
+        pass <- getPassword
+        return pass
+    else
+        return pass
+
+getLineSecret :: IO String
+getLineSecret = do
+    c <- getChar
+    if c == '\n' then 
+        return []
+     else
+         do cs <- getLineSecret
+            return (c:cs)
+
+chunk :: BC.ByteString -> [BC.ByteString]
+chunk bytestring  = if BC.length bytestring == 0 then
+                        []
+                    else [BC.take 8 bytestring] ++ (chunk $ BC.drop 8 bytestring)
+
+pad :: [BC.ByteString] -> [BC.ByteString]
+pad xs =  
+    if B.length finalBlock == 8 then
+        let padArray = 1:(take 7 $ repeat 0)
+        in  xs ++ [B.pack padArray]
+    else
+        let numZeroesNeeded = 7 - finalBlockLen
+            padArray = 128:(take numZeroesNeeded $ repeat 0)
+        in  (init xs) ++ [B.append finalBlock (B.pack padArray)]
+    where finalBlock = last xs
+          finalBlockLen = B.length finalBlock
+
+unpad :: [BC.ByteString] -> [BC.ByteString]
+unpad xs = 
+   (init xs) ++ [origBytes] -- orig bytes could be empty in the case of a perfect block size
+   where finalBlock = last xs
+         origBytes = B.reverse $ B.drop 1 $ B.dropWhile (0 ==) $ B.reverse finalBlock
